e1bf04536be2d356d0b170bdbe8ec19252000b05
Leamsy Heitor <leamsyheitor@hotmail.com>
2019-04-15 14:13:28 -0300

    AppTarefas - em Desenvolvimento.

---
+++ Tarefas/App2_Tarefa/App2_Tarefa/App2_Tarefa/Telas/Cadastro.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_Tarefa.Telas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Cadastro : ContentPage
+	{
+		public Cadastro ()
+		{
+			InitializeComponent ();
+		}
+        public void PrioridadeSelection(object sender, EventArgs Args)
+        {
+            var Stacks = SLPrioridades.Children;
+
+            foreach (var Linha in Stacks)
+            {
+                Label LblPrioridade = ((StackLayout)Linha).Children[1] as Label;
+                LblPrioridade.TextColor = Color.Gray;
+            }
+
+            ((Label)((StackLayout)sender).Children[1]).TextColor = Color.Black;
+        }
+
+    }
+}
